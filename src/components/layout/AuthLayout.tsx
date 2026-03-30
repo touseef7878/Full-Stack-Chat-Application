59239f6fff4e958441787888aa1c0704de62bcbf
Touseef Ur Rehman <touseefurrehman5554@gmail.com>
2026-03-30 10:03:46 +0500

    Removed background image

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -5,17 +5,7 @@ interface AuthLayoutProps {
 }
 
 const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
-  return (
-    <div
-      className="min-h-screen w-full flex items-center justify-center bg-cover bg-center"
-      style={{
-        backgroundImage:
-          "url('https://img.freepik.com/free-vector/dialogue-chat-clouds-speech-bubble-icon-from-lines-triangles-particle-style-design-low-poly-technology-devices-people-communication-concept-blue-background_587448-471.jpg')",
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 };
 
 export default AuthLayout;
